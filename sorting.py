d0f1d92a31fe85d10e11311feedfb592c518b8cc
munna <munnakumar5461@gmail.com>
2018-07-07 22:03:08 +0530

    sorting algorithms

---
+++ sorting.py
@@ -0,0 +1,120 @@
+
+# coding: utf-8
+
+# In[8]:
+
+# bubble sort
+def bubbleSort(a):
+    for i in range(len(a)-1):
+        for j in range(len(a)-i-1):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+                
+    return a
+#sortedlist=bubbleSort()
+#selection sort
+
+def selectionSort(a):
+    for i in range(0,len(a)-1):#because last element will be sorted 
+        min=i
+        for j in range(i+1,len(a)):
+            if a[j]<a[min]:
+                min =j
+        if min !=i:
+            a[i],a[min]=a[min],a[i]
+    return a
+#insertion sort
+
+def insertionSort(a):
+    for i in range(1,len(a)):
+        #first is starting,second is end exclude means it will go to -1+1=0,and last is step with -1
+        for j in range(i-1,-1,-1):
+            if(a[j]>a[j+1]):
+                a[j],a[j+1]=a[j+1],a[j]
+            else:
+                break
+    return a
+
+
+# shifting method of insertion sort
+def insertionSort_shifting(a):
+    for i in range(1,len(a)):
+        temp=a[i]
+        for j in range(i-1,-1,-1):
+            #print (j)
+            if a[j]>temp:
+                a[j+1]=a[j]
+                j=j-1 #this is required because 
+            else:
+                break
+            
+        a[j+1]=temp       
+    return a
+
+
+#---merge sort using recursion--------
+def mergesort(listValue):
+    if len(listValue)>1:
+        mid=len(listValue)//2
+        left=listValue[:mid]
+        right=listValue[mid:]
+        mergesort(left)
+        mergesort(right)
+        i=j=k=0
+        # this is used to merge to sorted listin one big sorted list
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                listValue[k]=left[i]
+                i=i+1
+            else:
+                listValue[k]=right[j]
+                j=j+1
+            k=k+1
+        #when above while loop and condiion fails means either left or right is finished so other will insert as it is 
+        while (i<len(left)):
+            listValue[k]=left[i]
+            i=i+1
+            k=k+1
+        while(j<len(right)):
+            listValue[k]=right[j]
+            j=j+1
+            k=k+1
+    return listValue
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
